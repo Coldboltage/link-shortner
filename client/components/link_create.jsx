5933cb3b5cf258b948c84775625f80dfe226db23
Coldboltage <alandreid@hotmail.co.uk>
2020-10-05 00:36:34 +0100

    General Push
    Think this should add everything to this git. Who knows.

---
+++ client/components/link_create.jsx
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from "react";
+
+const LinkCreate = () => {
+  // Create State
+  const [error, setError] = useState("")
+
+  // Create reference 
+  const inputEl = useRef("");
+  const handleSubmit = event => {
+    // Validate the URL
+    Meteor.call("links.insert", inputEl.current.value, error => {
+      if (error) {
+        setError(`This is an invalid URL`);
+        inputEl.current.value = "";
+      } else {
+        setError("");
+      }
+    })
+
+    event.preventDefault()
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="">Link to shorten</label>
+        <input ref={inputEl} type="text" className="form-control" />
+      </div>
+      <div className="text-danger">{error}</div>
+      <button className="btn btn-primary">Shorten!</button>
+    </form>
+  )
+}
+
+export default LinkCreate
