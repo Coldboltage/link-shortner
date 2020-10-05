5b1bf29fa084ce192885cb1ae1961d4114fe0dfa
Coldboltage <alandreid@hotmail.co.uk>
2020-10-05 22:59:38 +0100

    added linkList component, react-meteor-data dependency
    publication added to the linkList using withTracker as the container to connect to links publication

--- client/components/link_list.jsx
+++ client/components/link_list.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+import { withTracker } from "meteor/react-meteor-data";
+import { Links } from "../../imports/collections/links"
+
 
 const linkList = () => {
   return (
     <div>List of links to click</div>
   )
 }
+
+export default withTracker(() => {
+  Meteor.subscribe("links");
+  return { links: Links.find({}).fetch()}
+}) (linkList); // Link List is the component we're injecting the publication into
