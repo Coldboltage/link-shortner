df57837e0a1cdc8b38d5b68cc8378ec5e2fbbfcb
Coldboltage <alandreid@hotmail.co.uk>
2020-10-06 20:45:00 +0100

    connect-route dependency & url routing
    I've added connect-route to look for everything past / for the token and if it has the token, to reroute

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Links } from "../imports/collections/links"
+import { WebApp } from "meteor/webapp";
+import ConnectRoute from "connect-route"
+
 
 
 Meteor.startup(() => {
@@ -7,3 +10,24 @@ Meteor.startup(() => {
     return Links.find({});
   })
 });
+
+
+// Executed whenever a user visits correctly
+const onRoute = (req, res, next) => {
+  // Get Token and find link in link collection
+  const link = Links.findOne( { token: req.params.token })
+  // If Link token is found, send them to long URL boom
+  if (link) {
+    res.writeHead(307, {"Location": link.url});
+    res.end();
+  } else {
+  // If we don't find a link object, send to react website
+    next();
+  }
+}
+
+const middleWear = ConnectRoute(function(router) {
+  router.get("/:token", onRoute)
+});
+
+WebApp.connectHandlers.use(middleWear);
